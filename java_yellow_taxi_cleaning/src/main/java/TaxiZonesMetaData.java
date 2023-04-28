a6fca7159faad6c9650f27c6141d0a7efa58b540
Mitchell Merrick-Thirlway <mm12063@nyu.edu>
2023-04-27 23:47:57 -0400

    Downgraded to javac 8 so changing back

--- java_yellow_taxi_cleaning/src/main/java/TaxiZonesMetaData.java
+++ java_yellow_taxi_cleaning/src/main/java/TaxiZonesMetaData.java
@@ -1,25 +1,48 @@
 public class TaxiZonesMetaData {
-    public static int getColIdx(ColNames col_name){
-        return switch (col_name) {
-            case VENDOR_ID -> 0;
-            case TPEP_PICKUP_DATETIME -> 1;
-            case TPEP_DROPOFF_DATETIME -> 2;
-            case PASSENGER_COUNT -> 3;
-            case TRIP_DISTANCE -> 4;
-            case RATECODE_ID -> 5;
-            case STORE_AND_FWD_FLAG -> 6;
-            case PU_LOCATION_ID -> 7;
-            case DO_LOCATION_ID -> 8;
-            case PAYMENT_TYPE -> 9;
-            case FARE_AMOUNT -> 10;
-            case EXTRA -> 11;
-            case MTA_TAX -> 12;
-            case TIP_AMOUNT -> 13;
-            case TOLLS_AMOUNT -> 14;
-            case IMPROVEMENT_SURCHARGE -> 15;
-            case TOTAL_AMOUNT -> 16;
-            case CONGESTION_CHARGE -> 17;
-            case AIRPORT_FEE -> 18;
-        };
+    public static int getColIdx(ColNames col_name) {
+        int colIdx;
+        switch (col_name) {
+            case VENDOR_ID: colIdx = 0;
+                break;
+            case TPEP_PICKUP_DATETIME: colIdx = 1;
+                break;
+            case TPEP_DROPOFF_DATETIME: colIdx = 2;
+                break;
+            case PASSENGER_COUNT: colIdx = 3;
+                break;
+            case TRIP_DISTANCE: colIdx = 4;
+                break;
+            case RATECODE_ID: colIdx = 5;
+                break;
+            case STORE_AND_FWD_FLAG: colIdx = 6;
+                break;
+            case PU_LOCATION_ID: colIdx = 7;
+                break;
+            case DO_LOCATION_ID: colIdx = 8;
+                break;
+            case PAYMENT_TYPE: colIdx = 9;
+                break;
+            case FARE_AMOUNT: colIdx = 10;
+                break;
+            case EXTRA: colIdx = 11;
+                break;
+            case MTA_TAX: colIdx = 12;
+                break;
+            case TIP_AMOUNT: colIdx = 13;
+                break;
+            case TOLLS_AMOUNT: colIdx = 14;
+                break;
+            case IMPROVEMENT_SURCHARGE: colIdx = 15;
+                break;
+            case TOTAL_AMOUNT: colIdx = 16;
+                break;
+            case CONGESTION_CHARGE: colIdx = 17;
+                break;
+            case AIRPORT_FEE: colIdx = 18;
+                break;
+            default: colIdx = -1;
+                break;
+        }
+        return colIdx;
     }
 }
