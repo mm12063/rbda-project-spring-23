59c132a33ae8f67c649c46097b30efb88ab89359
Mitchell Merrick-Thirlway <mm12063@nyu.edu>
2023-05-03 16:02:42 -0400

    Added rush hour sql

---
+++ sql_files/rush_hour.sql
@@ -0,0 +1,2 @@
+SELECT COUNT(*) as 2015_1_htp FROM yellow_taxi WHERE pu_year = 2015 AND pu_month = 1 AND htp_am = 1 OR htp_pm = 1; 
+SELECT COUNT(*) as 2015_2_htp FROM yellow_taxi WHERE pu_year = 2015 AND pu_month = 2 AND htp_am = 1 OR htp_pm = 1; 
--- sql_query_file_creator.py
+++ sql_query_file_creator.py
@@ -1,25 +1,41 @@
 import os
 
+# presto --catalog hive --schema default --file sql_files/XXXX.sql > sql_output/XXXX-output.txt
+
 MAIN_DIR = 'sql_files'
 if not os.path.exists(MAIN_DIR):
     os.mkdir(MAIN_DIR)
 
-output_file = f"./{MAIN_DIR}/seasons.sql"
-
 YEAR_START = 2015
-YEAR_END = 2021
+YEAR_END = 2015
+MONTH_START = 1
+MONTH_END = 2
+
+# output_file = f"./{MAIN_DIR}/seasons.sql"
+# if os.path.exists(output_file):
+#     os.remove(output_file)
+#
+# with open(output_file, "a") as file:
+#     for year in range(YEAR_START, YEAR_END+1):
+#         command = f"SELECT COUNT(*) FROM yellow_taxi WHERE pu_year = {year} AND pu_month = 3 OR pu_month = 4 OR pu_month = 5; \n"
+#         command += f"SELECT COUNT(*) FROM yellow_taxi WHERE pu_year = {year} AND pu_month = 6 OR pu_month = 7 OR pu_month = 8; \n"
+#         command += f"SELECT COUNT(*) FROM yellow_taxi WHERE pu_year = {year} AND pu_month = 9 OR pu_month = 10 OR pu_month = 11; \n"
+#         command += f"SELECT COUNT(*) FROM yellow_taxi WHERE pu_year = {year} AND pu_month = 12 OR pu_month = 1 OR pu_month = 2; \n"
+#         file.write(command)
+# file.close()
+
+
+
+output_file = f"./{MAIN_DIR}/rush_hour.sql"
+if os.path.exists(output_file):
+    os.remove(output_file)
 
 with open(output_file, "a") as file:
-    file.write("\n")
     for year in range(YEAR_START, YEAR_END+1):
-        command = f"SELECT COUNT(*) FROM yellow_taxi WHERE pu_year = {year} AND pu_month = 3 OR pu_month = 4 OR pu_month = 5;"
-        file.write(command)
-        command = f"SELECT COUNT(*) FROM yellow_taxi WHERE pu_year = {year} AND pu_month = 6 OR pu_month = 7 OR pu_month = 8;"
-        file.write(command)
-        command = f"SELECT COUNT(*) FROM yellow_taxi WHERE pu_year = {year} AND pu_month = 9 OR pu_month = 10 OR pu_month = 11;"
-        file.write(command)
-        command = f"SELECT COUNT(*) FROM yellow_taxi WHERE pu_year = {year} AND pu_month = 12 OR pu_month = 1 OR pu_month = 2;"
-        file.write(command)
+        for month in range(MONTH_START, MONTH_END+1):
+            command = f"SELECT COUNT(*) as {year}_{month}_htp FROM yellow_taxi WHERE pu_year = {year} AND pu_month = {month} AND htp_am = 1 OR htp_pm = 1; \n"
+            file.write(command)
 file.close()
+
 
 
