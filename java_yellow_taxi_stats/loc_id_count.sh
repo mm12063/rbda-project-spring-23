e30d4e6469208a76b65e1f5d34265c64e2367f23
Mitch <mm12063@nyu.edu>
2023-05-05 23:10:47 +0000

    SQL output results

--- java_yellow_taxi_stats/loc_id_count.sh
+++ java_yellow_taxi_stats/loc_id_count.sh
@@ -1,4 +1,4 @@
 mvn package
 hadoop fs -rm -r hdfs://nyu-dataproc-m/user/mm12063_nyu_edu/project/output_loc_id_count/
-hadoop jar ~/rbda-project-spring-23/java_yellow_taxi_cleaning/target/YellowTaxiStats-1.0.jar YellowTaxiStats hdfs://nyu-dataproc-m/user/mm12063_nyu_edu/project/output_yellow_taxi_manhattan/ hdfs://nyu-dataproc-m/user/mm12063_nyu_edu/project/output_loc_id_count
+hadoop jar ~/rbda-project-spring-23/java_yellow_taxi_stats/target/YellowTaxiStats-1.0.jar YellowTaxiStats hdfs://nyu-dataproc-m/user/mm12063_nyu_edu/project/output_yellow_taxi_manhattan/ hdfs://nyu-dataproc-m/user/mm12063_nyu_edu/project/output_loc_id_count
 hadoop fs -head hdfs://nyu-dataproc-m/user/mm12063_nyu_edu/project/output_loc_id_count/part-m-00000
