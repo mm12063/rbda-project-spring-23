c19952cc0198deb50b81f75e4f75227c75a03bb5
Mitchell Merrick-Thirlway <mm12063@nyu.edu>
2023-05-05 14:53:12 -0400

    Round avg values to 2 decimal places

--- java_yellow_taxi_stats/src/main/java/taxi/YellowTaxiStatsTuple.java
+++ java_yellow_taxi_stats/src/main/java/taxi/YellowTaxiStatsTuple.java
@@ -18,11 +18,11 @@ public class YellowTaxiStatsTuple implements Writable {
     public double getMin() {return min;}
     public void setMax(double max) {this.max = max;}
     public double getMax() {return max;}
-    public void setDistAvg(double dist_avg) {this.dist_avg = dist_avg;}
+    public void setDistAvg(double dist_avg) {this.dist_avg = Math.round(dist_avg * 100.0) / 100.0;;}
     public double getDistAvg() {return dist_avg;}
-    public void setPassAvg(double pass_avg) {this.pass_avg = pass_avg;}
+    public void setPassAvg(double pass_avg) {this.pass_avg = Math.round(pass_avg * 100.0) / 100.0;}
     public double getPassAvg() {return pass_avg;}
-    public void setCostAvg(double cost_avg) {this.cost_avg = cost_avg;}
+    public void setCostAvg(double cost_avg) {this.cost_avg = Math.round(cost_avg * 100.0) / 100.0;}
     public double getCostAvg() {return cost_avg;}
     public void setCount(long count) {this.count = count;}
     public long getCount() {return count;}
