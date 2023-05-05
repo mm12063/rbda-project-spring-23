0a3153418e96edd90cf57a0262c72bda7268780f
Mitchell Merrick-Thirlway <mm12063@nyu.edu>
2023-05-05 11:12:41 -0400

    Top 10 trips each year

--- java_yellow_taxi_stats/src/main/java/taxi/YellowTaxiStatsCountMapper.java
+++ java_yellow_taxi_stats/src/main/java/taxi/YellowTaxiStatsCountMapper.java
@@ -15,6 +15,14 @@ public class YellowTaxiStatsCountMapper extends Mapper<LongWritable, Text, Text,
         year = context.getConfiguration().get("year");
     }
 
+    private String clean_location_str(String in){
+        in = in.replace(" ","_");
+        in = in.replace("/","__");
+        in = in.replace("'","");
+        in = in.replace("-","");
+        return in;
+    }
+
     @Override
     public void map(LongWritable key, Text value, Context context)
             throws IOException, InterruptedException {
@@ -22,14 +30,14 @@ public class YellowTaxiStatsCountMapper extends Mapper<LongWritable, Text, Text,
         String[] values = value.toString().split(",");
         if (!values[0].equals("pu_date")) {
             try {
-                String pu_loc = values[CleanedCSVMetaData.getColIdx(ColNames.DO_TAXI_ZONE)];
-                pu_loc = pu_loc.replace(" ","_");
-                pu_loc = pu_loc.replace("/","__");
-                pu_loc = pu_loc.replace("'","");
-                pu_loc = pu_loc.replace("-","");
+                String pu_loc = values[CleanedCSVMetaData.getColIdx(ColNames.PU_TAXI_ZONE)];
+                pu_loc = clean_location_str(pu_loc);
+                String do_loc = values[CleanedCSVMetaData.getColIdx(ColNames.DO_TAXI_ZONE)];
+                do_loc = clean_location_str(do_loc);
+
                 String pu_year = values[CleanedCSVMetaData.getColIdx(ColNames.PU_YEAR)];
                 if (year.equals(pu_year))
-                    context.write(new Text(pu_loc), new IntWritable(1));
+                    context.write(new Text(pu_loc+"___"+do_loc), new IntWritable(1));
             } catch (Exception e) {
                 System.out.println("Couldn't read DO_TAXI_ZONE value from csv");
                 System.out.println(e.getMessage());
