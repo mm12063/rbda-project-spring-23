26d0a6ea9c9d5d826190566ee7ad6c71971d536b
Mitchell Merrick-Thirlway <mm12063@nyu.edu>
2023-05-05 16:04:08 -0400

    Updating dist queries

---
+++ sql_files/dist_avg_per_month.sql
@@ -0,0 +1,7 @@
+SELECT AVG(dist) FROM yellow_taxi WHERE pu_year = 2015; 
+SELECT AVG(dist) FROM yellow_taxi WHERE pu_year = 2016; 
+SELECT AVG(dist) FROM yellow_taxi WHERE pu_year = 2017; 
+SELECT AVG(dist) FROM yellow_taxi WHERE pu_year = 2018; 
+SELECT AVG(dist) FROM yellow_taxi WHERE pu_year = 2019; 
+SELECT AVG(dist) FROM yellow_taxi WHERE pu_year = 2020; 
+SELECT AVG(dist) FROM yellow_taxi WHERE pu_year = 2021; 
