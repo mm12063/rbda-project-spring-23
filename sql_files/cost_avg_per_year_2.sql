b80cd5bc08b30b04fcdc5bfecdf7f14b75359f6b
Mitchell Merrick-Thirlway <mm12063@nyu.edu>
2023-05-07 09:57:20 -0400

    limiting the value of cost in avg query'

---
+++ sql_files/cost_avg_per_year_2.sql
@@ -0,0 +1,7 @@
+SELECT AVG(cost) FROM yellow_taxi WHERE cost < 100 AND pu_year = 2015; 
+SELECT AVG(cost) FROM yellow_taxi WHERE cost < 100 AND pu_year = 2016; 
+SELECT AVG(cost) FROM yellow_taxi WHERE cost < 100 AND pu_year = 2017; 
+SELECT AVG(cost) FROM yellow_taxi WHERE cost < 100 AND pu_year = 2018; 
+SELECT AVG(cost) FROM yellow_taxi WHERE cost < 100 AND pu_year = 2019; 
+SELECT AVG(cost) FROM yellow_taxi WHERE cost < 100 AND pu_year = 2020; 
+SELECT AVG(cost) FROM yellow_taxi WHERE cost < 100 AND pu_year = 2021; 
