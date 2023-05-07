0d35158faaba26533f1920dcb79a2be53a437d1a
Mitchell Merrick-Thirlway <mm12063@nyu.edu>
2023-05-07 10:34:28 -0400

    Limiting dist and cost

---
+++ sql_files/basic_stats.sql
@@ -0,0 +1,6 @@
+select COUNT(*) from yellow_taxi;
+select AVG(passengers) from yellow_taxi;
+select AVG(cost) from yellow_taxi;
+select MIN(dist) from yellow_taxi;
+select MAX(dist) from yellow_taxi;
+select AVG(dist) from yellow_taxi;
---
+++ sql_files/dist_avg_per_year_2.sql
@@ -0,0 +1,7 @@
+SELECT AVG(dist) FROM yellow_taxi WHERE pu_year = 2015 AND dist < 6; 
+SELECT AVG(dist) FROM yellow_taxi WHERE pu_year = 2016 AND dist < 6; 
+SELECT AVG(dist) FROM yellow_taxi WHERE pu_year = 2017 AND dist < 6; 
+SELECT AVG(dist) FROM yellow_taxi WHERE pu_year = 2018 AND dist < 6; 
+SELECT AVG(dist) FROM yellow_taxi WHERE pu_year = 2019 AND dist < 6; 
+SELECT AVG(dist) FROM yellow_taxi WHERE pu_year = 2020 AND dist < 6; 
+SELECT AVG(dist) FROM yellow_taxi WHERE pu_year = 2021 AND dist < 6; 
